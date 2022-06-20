31611db8a29d1239ff39468d333edba126acf18a
mmaslan <marek.maslany1@gmail.com>
2022-06-20 21:55:33 +0200

    Creating connections via Airflow

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,11 +1,36 @@
-import airflow
+import os
+import json
 from airflow import DAG
-from airflow.operations.bash import BashOperator
-from airflow.operations.python import PythonOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+api_key = "588237482f885cef420a4fa9796cd1b9"
 
 
-dag = DAG(
-    dag_id='download_weather_data',
-    start_date=now,
-    schedule_interval='@hourly'
-)
+def save_posts(ti) -> None:
+    posts = ti.xcom_pull(task_ids=['get_posts'])
+    with open('C:\\Users\\marek\\Desktop\\Data\\posts.json', w) as f:
+        json.dump(posts[0], f)
+
+
+with DAG(
+        dag_id='weather_download',
+        schedule_interval='@hourly',
+        start_date=datetime(2022, 6, 25),
+        catchup=False,
+) as dag:
+    task_is_api_active = HttpSensor(
+        task_id='is_api_active',
+        http_conn_id='api_posts',
+        endpoint='posts/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+
+    task_save=PythonOperator(
+        task_id='save_posts',
+        python_callable=save_posts
+    )
