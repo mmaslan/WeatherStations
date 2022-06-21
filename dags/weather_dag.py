25508471cb9c581df93c02cbe4f678d0ba95a29b
mmaslan <marek.maslany1@gmail.com>
2022-06-21 13:34:26 +0200

    New functions

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,36 +1,28 @@
 import os
 import json
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-api_key = "588237482f885cef420a4fa9796cd1b9"
+dag = DAG(
+    dag_id='weather_load_dag',
+    # default_args=default_args,
+    start_date=datetime(2022, 6, 24),
+    schedule_interval='@hourly'
+)
 
+task1 = BashOperator(
+    task_id='get_weather',
+    dag=dag
+)
 
-def save_posts(ti) -> None:
-    posts = ti.xcom_pull(task_ids=['get_posts'])
-    with open('C:\\Users\\marek\\Desktop\\Data\\posts.json', w) as f:
-        json.dump(posts[0], f)
+task2 = PythonOperator(
+    task_id='transform_data',
+    provide_context=True,
+    dag=dag
+)
 
-
-with DAG(
-        dag_id='weather_download',
-        schedule_interval='@hourly',
-        start_date=datetime(2022, 6, 25),
-        catchup=False,
-) as dag:
-    task_is_api_active = HttpSensor(
-        task_id='is_api_active',
-        http_conn_id='api_posts',
-        endpoint='posts/',
-        method='GET',
-        response_filter=lambda response: json.loads(response.text),
-        log_response=True
-    )
-
-    task_save=PythonOperator(
-        task_id='save_posts',
-        python_callable=save_posts
-    )
+task1 >> task2
