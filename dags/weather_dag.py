690da3b91cbf6a9fee7ef47bf91787b79b8fa762
mmaslan <marek.maslany1@gmail.com>
2022-06-24 01:40:37 +0200

    Update

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -44,7 +44,6 @@ def createDirectory():
 
 
 def measure_mean(city):
-    # with open(f'C:\\Users\\marek\\Desktop\\WeatherStationsTest\\dags\\data\\weather_{city}_{day}.json', 'r') as file2:
     filename = f'data/weather_{city}_{day}.json'
     with open(os.path.join(filename), 'r') as file2:
         data = json.load(file2)
